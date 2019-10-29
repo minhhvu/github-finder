2177541cc591b2308fb3775b0dd31a0b1fb5420c
minhh <minhhaivuhd@gmail.com>
2019-10-29 12:26:02 -0400

    Initial files

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component{
+    render() {
+      return (
+          <div className="h1 text-center">
+              Hello World
+          </div>
+      );
+    }
+
+}
+
+export default App;
