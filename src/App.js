39f9e8784dd2727ccf57f4e6d220b78d18bc089a
minhh <minhhaivuhd@gmail.com>
2019-10-30 11:28:04 -0400

    modify the states to the root components

--- src/App.js
+++ src/App.js
@@ -4,15 +4,52 @@ import About from "./components/pages/About";
 import Homepage from "./components/pages/Homepage";
 import './App.css';
 import Navbar from "./components/layout/Navbar";
+import axios from "axios";
+import SearchForm from "./components/layout/SearchForm";
 
 class App extends React.Component{
+
+    state = {
+        keywords: '',
+        users: [],
+        loading: false,
+    };
+
+    async componentDidMount() {
+        //Initialize the users data
+        let res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+        this.setState({users: res.data});
+    }
+
+    handleKeywordsChange = (text) => {
+        this.setState({keywords : text});
+        // console.log(this.state.keywords);
+    }
+
+    handleUsersChange = async () => {
+        //Retrieve data from Github API
+        this.setState({loading: true});
+        const search = this.state.keywords !=='' ? this.state.keywords : 'minh'; //setup the 'minh' as the searching keywords
+        let response = await axios.get(`https://api.github.com/search/users?q=${search}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+
+        this.setState({users : response.data.items, loading: false});
+        // console.log(this.state.users);
+    }
+
 
     render() {
       return (
           <Router>
               <Navbar/>
               <Switch>
-                  <Route exact path='/' component={Homepage}/>
+                  <Route exact path='/' render={props => (
+                      <Homepage
+                          keywords={this.state.keywords}
+                          onKeywordsChange={this.handleKeywordsChange}
+                          onFormSubmit={this.handleUsersChange}
+                          users={this.state.users}
+                      />
+                  )}/>
                   <Route exact path={'/about'} component={About}/>
               </Switch>
           </Router>
--- src/components/pages/Homepage.js
+++ src/components/pages/Homepage.js
@@ -5,47 +5,17 @@ import UsersGrid from "../layout/UsersGrid";
 import axios from 'axios';
 
 class Homepage extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            keywords: '',
-            users: [],
-            loading: false,
-        };
-    }
-
-    async componentDidMount() {
-        //Initialize the users data
-        let res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-        this.setState({users: res.data});
-    }
-
-    handleKeywordsChange = (text) => {
-        this.setState({keywords : text});
-        // console.log(this.state.keywords);
-    }
-
-    handleUsersChange = async () => {
-        //Retrieve data from Github API
-        this.setState({loading: true});
-        const search = this.state.keywords !=='' ? this.state.keywords : 'minh'; //setup the 'minh' as the searching keywords
-        let response = await axios.get(`https://api.github.com/search/users?q=${search}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-
-        this.setState({users : response.data.items, loading: false});
-        // console.log(this.state.users);
-    }
 
     render() {
         return (
             <Fragment>
                 <SearchForm
-                    keywords={this.state.keywords}
-                    onKeywordsChange={this.handleKeywordsChange}
-                    onFormSubmit={this.handleUsersChange}
+                    keywords={this.props.keywords}
+                    onKeywordsChange={this.props.onKeywordsChange}
+                    onFormSubmit={this.props.onFormSubmit}
                 />
                 <UsersGrid
-                    users={this.state.users}
-                    // keywords={this.state.keywords}
+                    users={this.props.users}
                 />
             </Fragment>
         );
